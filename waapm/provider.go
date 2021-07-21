d8abc246b789d796492c8a78b016cdbb5bcb9457
empierre <epierre@e-nef.com>
2021-07-21 15:38:59 +0200

    paths updates

--- waapm/provider.go
+++ waapm/provider.go
@@ -7,8 +7,8 @@ import (
 	"os/exec"
 	"strconv"
 
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+    	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 // Provider implementation for waapm
